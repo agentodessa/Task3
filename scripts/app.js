4325fc85b605d3350764ae092b53fc9030212192
agentodessa <agentodessa2011@gmail.com>
2014-08-18 18:00:56 +0300

    Adding

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+
+    $(".input_name").click(function() {
+        $(this).val("");
+    });
+	$(".login_btn").click(function () {
+
+	});
+
+
+	$(".login_anonymous").click(function () {
+		$(".login_page").css('text-indent', '');
+		$("body").css('background-color', 'white');
+	});
+})
