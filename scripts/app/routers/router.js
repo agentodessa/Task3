cf21f96a1f0d07d0df2330eb430f059e04a348fe
agentodessa <agentodessa2011@gmail.com>
2014-08-26 19:16:27 +0300

    Router Trying

---
+++ scripts/app/routers/router.js
@@ -0,0 +1,42 @@
+﻿var app = app || {};
+
+
+(function () {
+	var ComissioinRouter = Backbone.Router.extend({
+		routes:
+		{
+			"mobility": "maintain",  //#maintain
+			"updownload": "updownload",//#updownload
+			"reports":"reports",//#reports
+			"maintenance":"maintenance"//#maintenance
+		},
+
+		//mobility:function (param) {
+		//	app.route();
+		//},
+		//tablet: function() {
+			
+		//},
+
+		//pc: function() {
+			
+		//}
+	});
+
+	var appRouter = new ComissioinRouter();
+
+	appRouter.on('route:maintain',function(page) {
+		app.MaintainView(page);
+	});
+	appRouter.on('route:updownload', function (page) {
+		app.UpDownloadView(page);
+	});
+	appRouter.on('route:reports', function (page) {
+		app.ReportsView(page);
+	});
+	appRouter.on('route:maintenance', function (page) {
+		app.MaintenanceView(page);
+	});
+
+	Backbone.history.start();
+})();
---
+++ scripts/app/views/ViewController.js
@@ -0,0 +1,49 @@
+﻿var app = app || {};
+
+
+(function($) {
+	app.MaintainView = Backbone.View.extend({
+		el: "#fragment-1",
+
+		initialize: function () {
+			this.$select = this.$(".select_type");
+		},
+
+		render:function() {
+			
+		}
+	});
+	app.UpDownloadView = Backbone.View.extend({
+		el: "#fragment-2",
+
+		initialize: function () {
+
+		},
+
+		render: function () {
+
+		}
+	});
+	app.ReportsView = Backbone.View.extend({
+		el: "#fragment-3",
+
+		initialize: function () {
+
+		},
+
+		render: function () {
+
+		}
+	});
+	app.MaintenanceView = Backbone.View.extend({
+		el: "#fragment-4",
+
+		initialize: function () {
+			route();
+		},
+
+		render: function () {
+
+		}
+	});
+})(jQuery);
