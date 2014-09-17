8a1c2ae2255ebaec6bf227c2eca01d9d89a89788
agentodessa <agentodessa2011@gmail.com>
2014-09-17 18:38:42 +0300

    Page-proof fixing

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function (grunt) {
 		},
 		watch: {
 			scripts: {
-				files: 'scripts/app/**/*.js',
+				files: ['scripts/app/**/*.js', 'css/**/*.css'],
 				tasks: ['jshint'],
 				options: {
 					interrupt: true,
