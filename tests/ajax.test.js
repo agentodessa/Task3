71394e4819aec0bc964953c7d5562f9bf2d35da7
agentodessa <agentodessa2011@gmail.com>
2014-09-18 17:00:11 +0300

    Fixing bugs with ajax test case

--- tests/ajax.test.js
+++ tests/ajax.test.js
@@ -1,100 +1,76 @@
 ﻿QUnit.module("Group E");
-QUnit.asyncTest("Ajax test: one second later!", function (assert) {
-	setTimeout(function () {	
+QUnit.asyncTest("Ajax test: one second later!", function(assert) {
+
 	app.getSourceType().done(function(data) {
 		assert.ok(data !== null, "Ajax getSourceType passed");
 	});
-	}, 1000);
-
-	setTimeout(function () {
-		app.getSourceType().error(function (data) {
-			assert.ok(data === null, "Ajax getSourceType failed");
-			QUnit.start();
-		});
-	}, 1000);
 
 
-	setTimeout(function () {
-		app.retrieveTierDetails().done(function (data) {
-			assert.ok(data !== null, "Ajax retrieveTierDetails passed");
-		});
-	}, 1000);
-
-	setTimeout(function () {
-		app.retrieveTierDetails().error(function (data) {
-			assert.ok(data === null, "Ajax retrieveTierDetails failed");
-			QUnit.start();
-		});
-	}, 1000);
+	app.getSourceType().error(function(data) {
+		assert.ok(data === null, "Ajax getSourceType failed");
+	});
 
 
-	setTimeout(function () {
-		app.retrieveTierDetailsMobile().done(function (data) {
-			assert.ok(data !== null, "Ajax retrieveTierDetailsMobile passed");
-		});
-	}, 1000);
+	app.retrieveTierDetails().done(function(data) {
+		assert.ok(data !== null, "Ajax retrieveTierDetails passed");
+	});
 
-	setTimeout(function () {
-		app.retrieveTierDetailsMobile().error(function (data) {
-			assert.ok(data === null, "Ajax retrieveTierDetailsMobile failed");
-			QUnit.start();
-		});
-
-	}, 1000);
+	app.retrieveTierDetails().error(function(data) {
+		assert.ok(data === null, "Ajax retrieveTierDetails failed");
+	});
 
 
-	setTimeout(function () {
-		app.retrieveTierDetailsSlam().done(function (data) {
-			assert.ok(data !== null, "Ajax retrieveTierDetailsSlam passed");
-		});
-	}, 1000);
-
-	setTimeout(function () {
-		app.retrieveTierDetailsSlam().error(function (data) {
-			assert.ok(data === null, "Ajax retrieveTierDetailsSlam failed");
-			QUnit.start();
-		});
-	}, 1000);
+	app.retrieveTierDetailsMobile().done(function(data) {
+		assert.ok(data !== null, "Ajax retrieveTierDetailsMobile passed");
+	});
 
 
-	setTimeout(function () {
-		app.validateTierMobile().done(function (data) {
-			assert.ok(data !== null, "Ajax validateTierMobile passed");
-		});
-	}, 1000);
+	app.retrieveTierDetailsMobile().error(function(data) {
+		assert.ok(data === null, "Ajax retrieveTierDetailsMobile failed");
 
-	setTimeout(function () {
-		app.validateTierMobile().error(function (data) {
-			assert.ok(data === null, "Ajax validateTierMobile failed");
-			QUnit.start();
-		});
-	}, 1000);
+	});
 
 
-	setTimeout(function () {
-		app.validateTierSlam().done(function (data) {
-			assert.ok(data !== null, "Ajax validateTierSlam passed");
-		});
-	}, 1000);
+	app.retrieveTierDetailsSlam().done(function(data) {
+		assert.ok(data !== null, "Ajax retrieveTierDetailsSlam passed");
+	});
 
-	setTimeout(function () {
-		app.validateTierSlam().error(function (data) {
-			assert.ok(data === null, "Ajax validateTierSlam failed");
-			QUnit.start();
-		});
-	}, 1000);
 
-	setTimeout(function () {
-		app.validateTier().done(function (data) {
-			assert.ok(data !== null, "Ajax validateTier passed");
-		});
-	}, 1000);
+	app.retrieveTierDetailsSlam().error(function(data) {
+		assert.ok(data === null, "Ajax retrieveTierDetailsSlam failed");
 
-	setTimeout(function () {
-		app.validateTier().error(function (data) {
-			assert.ok(data === null, "Ajax validateTier failed");
-			QUnit.start();
-		});
-	}, 1000);
+	});
+
+
+	app.validateTierMobile().done(function(data) {
+		assert.ok(data !== null, "Ajax validateTierMobile passed");
+	});
+
+	app.validateTierMobile().error(function(data) {
+		assert.ok(data === null, "Ajax validateTierMobile failed");
+
+	});
+
+
+	app.validateTierSlam().done(function(data) {
+		assert.ok(data !== null, "Ajax validateTierSlam passed");
+	});
+
+
+	app.validateTierSlam().error(function(data) {
+		assert.ok(data === null, "Ajax validateTierSlam failed");
+	});
+
+	app.validateTier().done(function(data) {
+		assert.ok(data !== null, "Ajax validateTier passed");
+	});
+
+
+	app.validateTier().error(function(data) {
+		assert.ok(data === null, "Ajax validateTier failed");
+	});
+
+	setTimeout(function() {
+		QUnit.start();
+	});
 });
-
